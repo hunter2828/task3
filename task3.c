21f9c6187cf5bab1b710a2eeaa3715989f9638bb
Hunter Bake-Smith <hunter.bakesmith@gmail.com>
2019-04-15 06:01:41 +0000

    task 3 in progress, getting output of BBBBBBBBBBB

--- task3.c
+++ task3.c
@@ -1,185 +1,213 @@
 #include <stdio.h>
+
 int main()
 {
-    char ch;
+    int n;
+    FILE *inputsk3;
+    inputsk3 = fopen("inputsk3.txt", "r");
+
     int getc(FILE *inputtsk3);
-   // char alphaorig[26] = {"Attack at noon"};
-   // char substitute[26] = {'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A'};
-   
-    
-    
-    FILE *inputtsk3;
-    inputtsk3 = fopen("inputtsk3.txt", "r");
-    
-    for(ch = 65; ch<67; ch++)
+    char alpha[26] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+    for(n = 0; n<26; n++)
     {
-        ch = fseek(inputtsk3, 1, 1);
-        printf("%c", ch);
-    }
+        if(inputsk3 != NULL)
+  {
+      alpha[n] = fgetc(inputsk3);
+      
+  }
+}
     
-  
+    FILE *tsk3orig;
+    tsk3orig = fopen("tsk3orig.txt", "r");
+    while(feof(tsk3orig) == 0)
+       { char ch = fgetc(tsk3orig);
+       
+        //fscanf(tsk3orig, "%c", &ch);
+        
+        if(ch = 'A')
+        {
+            ch = alpha[0];
+            
+        }
 
 
-      /*  if(ch = 'A')
+        else if(ch = 'B')
         {
-            ch = substitute[0];
+            ch = alpha[1];
+            
+        }
+        
+        
+        else if(ch = 'C')
+        {
+            ch = alpha[2];
+            
+        }
+        
+        
+        else if(ch = 'D')
+        {
+            ch = alpha[3];
+            
+        }
+        
+        
+        else if(ch = 'E')
+        {
+            ch = alpha[4];
+            
+        }
+        
+        
+        else if(ch = 'F')
+        {
+            ch = alpha[5];
+            
+        }
+        
+        
+        else if(ch = 'G')
+        {
+            ch = alpha[6];
+            
+        }
+        
+        
+        else if(ch = 'H')
+        {
+            ch = alpha[7];
+            
+        }
+        
+        
+        else if(ch = 'I')
+        {
+            ch = alpha[8];
+            
+        }
+        
+        
+        else if(ch = 'J')
+        {
+            ch = alpha[9];
+            
+        }
+        
+        
+        else if(ch = 'K')
+        {
+            ch = alpha[10];
+            
+        }
+        
+        
+        else if(ch = 'L')
+        {
+            ch = alpha[11];
+            
+        }
+        
+        
+        else if(ch = 'M')
+        {
+            ch = alpha[12];
+            
+        }
+        
+        
+        else if(ch = 'N')
+        {
+            ch = alpha[13];
+            
+        }
+        
+        
+        else if(ch = 'O')
+        {
+            ch = alpha[14];
+            
+        }
+        
+        
+        else if(ch = 'P')
+        {
+            ch = alpha[15];
+            
+        }
+        
+        
+        else if(ch = 'Q')
+        {
+            ch = alpha[16];
+            
+        }
+        
+        
+        else if(ch = 'R')
+        {
+            ch = alpha[17];
+            
+        }
+        
+        
+        else if(ch = 'S')
+        {
+            ch = alpha[18];
+            
+        }
+        
+        
+        else if(ch = 'T')
+        {
+            ch = alpha[19];
+            
+        }
+        
+        
+        else if(ch = 'U')
+        {
+            ch = alpha[20];
+            
+        }
+        
+        
+        else if(ch = 'V')
+        {
+            ch = alpha[21];
+            
+        }
+        
+        
+        else if(ch = 'W')
+        {
+            ch = alpha[22];
+            
+        }
+        
+        
+        else if(ch = 'X')
+        {
+            ch = alpha[23];
+            
+        }
+        
+        
+        else if(ch = 'Y')
+        {
+            ch = alpha[24];
+            
+        }
+        
+        
+        else if(ch = 'Z')
+        {
+            ch = alpha[25];
+            
         }
         printf("%c", ch);
-        
-        if(ch = 'B')
-        {
-            ch = substitute[1];
-        }
-        printf("%c", ch);
-        
-        if(ch = 'C')
-        {
-            ch = substitute[2];
-        }
-        printf("%c", ch);
-        
-        if(ch = 'D')
-        {
-            ch = substitute[3];
-        }
-        printf("%c", ch);
-        
-        if(ch = 'E')
-        {
-            ch = substitute[4];
-        }
-        printf("%c", ch);
-        
-        if(ch = 'F')
-        {
-            ch = substitute[5];
-        }
-        printf("%c", ch);
-        
-        if(ch = 'G')
-        {
-            ch = substitute[6];
-        }
-        printf("%c", ch);
-        
-        if(ch = 'H')
-        {
-            ch = substitute[7];
-        }
-        printf("%c", ch);
-        
-        if(ch = 'I')
-        {
-            ch = substitute[8];
-        }
-        printf("%c", ch);
-        
-        if(ch = 'J')
-        {
-            ch = substitute[9];
-        }
-        printf("%c", ch);
-        
-        if(ch = 'K')
-        {
-            ch = substitute[10];
-        }
-        printf("%c", ch);
-        
-        if(ch = 'L')
-        {
-            ch = substitute[11];
-        }
-        printf("%c", ch);
-        
-        if(ch = 'M')
-        {
-            ch = substitute[12];
-        }
-        printf("%c", ch);
-        
-        if(ch = 'N')
-        {
-            ch = substitute[13];
-        }
-        printf("%c", ch);
-        
-        if(ch = 'O')
-        {
-            ch = substitute[14];
-        }
-        printf("%c", ch);
-        
-        if(ch = 'P')
-        {
-            ch = substitute[15];
-        }
-        printf("%c", ch);
-        
-        if(ch = 'Q')
-        {
-            ch = substitute[16];
-        }
-        printf("%c", ch);
-        
-        if(ch = 'R')
-        {
-            ch = substitute[17];
-        }
-        printf("%c", ch);
-        
-        if(ch = 'S')
-        {
-            ch = substitute[18];
-        }
-        printf("%c", ch);
-        
-        if(ch = 'T')
-        {
-            ch = substitute[19];
-        }
-        printf("%c", ch);
-        
-        if(ch = 'U')
-        {
-            ch = substitute[20];
-        }
-        printf("%c", ch);
-        
-        if(ch = 'V')
-        {
-            ch = substitute[21];
-        }
-        printf("%c", ch);
-        
-        if(ch = 'W')
-        {
-            ch = substitute[22];
-        }
-        printf("%c", ch);
-        
-        if(ch = 'X')
-        {
-            ch = substitute[23];
-        }
-        printf("%c", ch);
-        
-        if(ch = 'Y')
-        {
-            ch = substitute[24];
-        }
-        printf("%c", ch);
-        
-        if(ch = 'Z')
-        {
-            ch = substitute[25];
-        }
-        printf("%c", ch);
-        
-        */
-    
+  }  
+
     return 0;
 }
 
-    
-    
+ 
