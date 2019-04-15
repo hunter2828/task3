419291fef970acc7d47f96e726412c2c9a61c5f5
Hunter Bake-Smith <hunter.bakesmith@gmail.com>
2019-04-15 08:24:25 +0000

    Working, nee to put files into folder for this to work

--- task3.c
+++ task3.c
@@ -5,201 +5,204 @@ int main()
     int n;
     FILE *inputsk3;
     inputsk3 = fopen("inputsk3.txt", "r");
-
-    int getc(FILE *inputtsk3);
+    char ch;
+    int fgetc(FILE *inputtsk3);
+    //char origalpha[26] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
     char alpha[26] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
     for(n = 0; n<26; n++)
     {
-        if(inputsk3 != NULL)
+        if(feof(inputsk3) == 0)
   {
       alpha[n] = fgetc(inputsk3);
       
+      
   }
 }
-    
-    FILE *tsk3orig;
+   
+    FILE *tsk3orig; //opening file with text to encrypt
     tsk3orig = fopen("tsk3orig.txt", "r");
-    while(feof(tsk3orig) == 0)
-       { char ch = fgetc(tsk3orig);
-       
+    int fgetc(FILE *tsk3orig);
+    while(feof(tsk3orig) == 0) // DO NOT USE FP != NULL, WILL CRASH WHILE LOOP
+          {
+              ch = fgetc(tsk3orig);
         //fscanf(tsk3orig, "%c", &ch);
         
-        if(ch = 'A')
+        if(ch == 'A')
         {
             ch = alpha[0];
             
         }
 
 
-        else if(ch = 'B')
+        else if(ch == 'B')
         {
             ch = alpha[1];
             
         }
         
         
-        else if(ch = 'C')
+        else if(ch == 'C')
         {
             ch = alpha[2];
             
         }
         
         
-        else if(ch = 'D')
+        else if(ch == 'D')
         {
             ch = alpha[3];
             
         }
         
         
-        else if(ch = 'E')
+        else if(ch == 'E')
         {
             ch = alpha[4];
             
         }
         
         
-        else if(ch = 'F')
+        else if(ch == 'F')
         {
             ch = alpha[5];
             
         }
         
         
-        else if(ch = 'G')
+        else if(ch == 'G')
         {
             ch = alpha[6];
             
         }
         
         
-        else if(ch = 'H')
+        else if(ch == 'H')
         {
             ch = alpha[7];
             
         }
         
         
-        else if(ch = 'I')
+        else if(ch == 'I')
         {
             ch = alpha[8];
             
         }
         
         
-        else if(ch = 'J')
+        else if(ch == 'J')
         {
             ch = alpha[9];
             
         }
         
         
-        else if(ch = 'K')
+        else if(ch == 'K')
         {
             ch = alpha[10];
             
         }
         
         
-        else if(ch = 'L')
+        else if(ch == 'L')
         {
             ch = alpha[11];
             
         }
         
         
-        else if(ch = 'M')
+        else if(ch == 'M')
         {
             ch = alpha[12];
             
         }
         
         
-        else if(ch = 'N')
+        else if(ch == 'N')
         {
             ch = alpha[13];
             
         }
         
         
-        else if(ch = 'O')
+        else if(ch == 'O')
         {
             ch = alpha[14];
             
         }
         
         
-        else if(ch = 'P')
+        else if(ch == 'P')
         {
             ch = alpha[15];
             
         }
         
         
-        else if(ch = 'Q')
+        else if(ch == 'Q')
         {
             ch = alpha[16];
             
         }
         
         
-        else if(ch = 'R')
+        else if(ch == 'R')
         {
             ch = alpha[17];
             
         }
         
         
-        else if(ch = 'S')
+        else if(ch =='S')
         {
             ch = alpha[18];
             
         }
         
         
-        else if(ch = 'T')
+        else if(ch == 'T')
         {
             ch = alpha[19];
             
         }
         
         
-        else if(ch = 'U')
+        else if(ch == 'U')
         {
             ch = alpha[20];
             
         }
         
         
-        else if(ch = 'V')
+        else if(ch == 'V')
         {
             ch = alpha[21];
             
         }
         
         
-        else if(ch = 'W')
+        else if(ch == 'W')
         {
             ch = alpha[22];
             
         }
         
         
-        else if(ch = 'X')
+        else if(ch == 'X')
         {
             ch = alpha[23];
             
         }
         
         
-        else if(ch = 'Y')
+        else if(ch == 'Y')
         {
             ch = alpha[24];
             
         }
         
         
-        else if(ch = 'Z')
+        else if(ch == 'Z')
         {
             ch = alpha[25];
             
