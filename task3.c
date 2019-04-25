a00a70facb152f289d77f6b0290cb0d775aae602
Hunter Bake-Smith <hunter.bakesmith@gmail.com>
2019-04-25 02:22:29 +0000

    TASK 1 working fully

--- task3.c
+++ task3.c
@@ -1,49 +1,65 @@
 #include <stdio.h>
 
-int main()
-{
-    int key;
-    char ch;
-    printf("Enter key: \n");
-    scanf("%d", &key);
-    //Write text to a file, then read it from a file, lecture 9/04 around half hour into lecture recording.
-    FILE *todecrypt5;
-    todecrypt5 = fopen("todecrypt5.txt", "r");
+int main() {
+     int key;
+        char ch;
+    FILE *todecrypt5;//initialises file pointer 
+        
+         char sentence[200]; //string to store the message in
+         
+        printf("Enter string: \n"); //prompts user to enter sentence
 
+        fgets(sentence, 200, stdin); //gets sentence from stdin and stores it as 'sentence'
+        todecrypt5 = fopen("todecrypt5.txt", "w"); //opens filefor writing
+        fputs(sentence, todecrypt5); //stores 'sentence' in the file
+
+        fclose(todecrypt5);//closes file
+        todecrypt5 = fopen("todecrypt5.txt", "r");//opens the file for reading
+        fscanf(todecrypt5, "%c", &ch); //scans the file for the first char and stores it as ch
+   
+        printf("Enter key: \n");// prompts user to enter key
+        scanf("%d", &key); //stores input as 'key'
+                
+	    printf("%c", sentence[0]+key);//prints the first char of the file and adds the key. This is done as the fgets fucntion was skipping sentence[1].
 
    
-       while(feof(todecrypt5) == 0)
-       
-       {   
-             
-                fscanf(todecrypt5, "%c", &ch);
-                if(ch>= 'a' && ch<= 'z')//if loop to encrypt lower case letters. The loop adds the key to the value, and if the value exceeds the value of lowercase 'z', it initiates another if loop.
+        
+      
+           while(feof(todecrypt5) == 0)
+           //while(i < 1)
+           {
+            //i++;
+            fscanf(todecrypt5, "%c", &ch);
+            if(ch>= 'a' && ch<= 'z')//if loop to encrypt lower case letters. The loop adds the key to the value, and if the value exceeds the value of lowercase 'z', it initiates another if loop.
+            {
+                ch = ch + key;//a letters ascii code plus the key is assigned to the same variable butwill have a different ascii code
+                if(ch > 'z')//used to encrypt an values that 'overflow' past the ascii value of 'z'
                 {
-                    ch = ch - key;//a letters ascii code plus the key is assigned to the same variable butwill have a different ascii code
-                    if(ch < 'a')//used to encrypt an values that 'overflow' past the ascii value of 'z'
-                    {
-                        ch = ch + 26;//formula encrypts values that overflow past z, by using previous value calculated for the new 'ch' and minuses 26.
-                    }   
+                    ch = ch - 26;//formula encrypts values that overflow past z, by using previous value calculated for the new 'ch' and minuses 26.
                 }
                 
-                else if(ch>= 'A' && ch<= 'Z')//if loop to encrypt lower case letters. The loop adds the key to the value, and if the value exceeds the value of uppercase 'Z' it initiates another loop
-                {
-                    ch = ch - key;//a letters ascii code plus the key is assigned to the same variable butwill have a different ascii code
-                    if(ch < 'A')//used to encrypt an values that 'overflow' past the ascii value of 'Z'
-                    {
-                        ch = ch + 26;
-                    }
-                    
-                }
-                printf("%c", ch);
                 
+            }
             
-        }
+            else if(ch>= 'A' && ch<= 'Z')//if loop to encrypt lower case letters. The loop adds the key to the value, and if the value exceeds the value of uppercase 'Z' it initiates another loop
+            {
+                ch = ch + key;//a letters ascii code plus the key is assigned to the same variable butwill have a different ascii code
+                if(ch > 'Z')//used to encrypt an values that 'overflow' past the ascii value of 'Z'
+                {
+                    ch = ch - 26;
+                }
+                
+            }
+            printf("%c", ch);
+           }
 
 
-
-
-    return 0;
+  
+	return 0;
 }
 
+
+
+
+ 
  
